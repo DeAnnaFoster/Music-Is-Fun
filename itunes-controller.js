7e34d17a2316a8b6b5a950ecf8d914e1fd291b4c
Dee Foster <dee3724f@gmail.com>
2017-08-04 02:09:26 -0600

    mostly done on Music project

--- itunes-controller.js
+++ itunes-controller.js
@@ -4,12 +4,20 @@ function ItunesController() {
   this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
+    storeArtist(artist);
     itunesService.getMusicByArtist(artist).then(drawSongs); //after get music by artist returns what are you doing with the objects?
   }
 
   //Start coding here
+  var searchArtist = '';
 
+  storeArtist = function (artist) {
+    searchArtist = artist;
+  }
 
+  getsearchArtist = function () {
+    return searchArtist;
+  }
 
   drawSongs = function (songList) {
     //adding the correct information to the screen
@@ -27,26 +35,83 @@ function ItunesController() {
     // }]
     // ```
 
-    console.log(songList);
+    //noticed videos have the term 'video' in preview and music has either 'music' or 'audio' in preview
 
-  }
+    let artst = getsearchArtist();
+    var mainTemplate = `
+        <h2>Summary of ${songList.length} Results for: ${artst}</h2>
+      `
+    document.getElementById('mainTitle').innerHTML = mainTemplate;
 
+    var template = '';
 
-  this.toggle = function () {
-    //debugger
-    var audioPlayer = document.getElementById('test');
+    for (var i = 0; i < songList.length; i++) {
+      var song = songList[i];
 
-    if (audioPlayer.paused) {
-      audioPlayer.play();
-      document.getElementById('test1').innerText = 'Stop';
-    } else {
-      audioPlayer.pause();
-      document.getElementById('test1').innerText = 'Play';
+      //truncate the 'By' Field
+      var by = ''
+      if (song.artist.length > 35) {
+        by = song.artist.substring(0, 35);
+      } else {
+        by = song.artist;
+      }
+
+      //filter here for vid vs. audio
+      let tempPreview = songList[i].preview.toLowerCase();
+      // if(tempPreview.indexOf('video')!= -1){
+      //   console.log(tempPreview);
+      // }
+
+      if (tempPreview.indexOf('video') == -1) {
+
+        template += `
+      <div class="card card-outline-primary">
+        <img class="card-img-top" src="${song.albumArt}" alt="song image">
+          <div class="card-block">
+            <p class="card-title">${song.title} from ${song.collection} (${song.price})</p>
+            <p>By: ${by}</p>
+            <audio class= "allAudios" id="ap${i}" src="${song.preview}" type="audio/wav"></audio>
+            <div>
+              <button type="button" id="btn${i}" class="btn btn-default" onclick="app.controllers.itunesCtrl.toggle(ap${i},btn${i})"><span class="glyphicon glyphicon-play" aria-hidden="true"></span></button>
+            </div>
+          </div>
+      </div>
+      `
+      }
     }
 
+    document.getElementById('templateInsert').innerHTML = template;
+    //songList = '';
   }
 
+  this.toggle = function (audioId, btn) {
+    var audioPlayer = document.getElementsByClassName('allAudios');
 
+    for (var i = 0; i < audioPlayer.length; i++) {
+      let ap = audioPlayer[i];
 
+      //maybe see if I can improve this a bit more...maybe loop through all to pause first, then play the one
 
+      if (ap.id != audioId.id) {
+        if (!ap.paused) {   //if its playing
+          ap.pause();       //pause it
+
+          document.getElementById(btn.id).innerHTML = '<span class="glyphicon glyphicon-play"></span>';
+
+        }
+      } else {
+        if (ap.paused) {      //if its paused
+          ap.play();        //play it
+
+          document.getElementById(btn.id).innerHTML = '<span class="glyphicon glyphicon-pause"></span>';
+
+        } else {
+          ap.pause();
+
+          document.getElementById(btn.id).innerHTML = '<span class="glyphicon glyphicon-play"></span>';
+
+        }
+      }
+    }
+  }
 }
